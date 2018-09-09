133bb34ee81a4aa8f0e5299cd381e8b24cadd8d2
Vlad Trishch <vlad.trishch@gmail.com>
2018-09-09 14:17:50 +0300

    Made Header mobile-friendly

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,10 @@
+const formatMedia= size => qualifier => `@media (${qualifier}-width: ${size}px)`
+
+const smMedia = formatMedia(576)
+const mdMedia = formatMedia(768)
+const lgMedia = formatMedia(992)
+const xlMedia = formatMedia(1200)
+
 export default {
   baseColor: '#242424',
   baseFontColor: 'white',
@@ -5,5 +12,11 @@ export default {
   highlightedColor: '#00f19f',
   defaultSection: {
     padding: '5% 0',
+  },
+  breakpoints: {
+    sm: { lt: smMedia('max'), gt: smMedia('min') },
+    md: { lt: mdMedia('max'), gt: mdMedia('min') },
+    lg: { lt: lgMedia('max'), gt: lgMedia('min') },
+    xl: { lt: xlMedia('max'), gt: xlMedia('min') },
   },
 }
