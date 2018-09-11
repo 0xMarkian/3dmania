7bb64936c0e3f499cf829fdfd3b791104db7c334
Vlad Trishch <vlad.trishch@gmail.com>
2018-09-11 18:18:41 +0300

    Added https support

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express')
+var fs = require('fs')
+var https = require('https')
+
 var app = express()
 
 app.use(express.static('build'))
 
-app.listen(80, function() {console.log('Listening on port 80...')})
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app).listen(80, function() {console.log('Listening on port 80...')})
