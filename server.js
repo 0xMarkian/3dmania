12dae13c3cb7e8e8333b5a801dec9e11b78368af
Vlad Trishch <vlad.trishch@gmail.com>
2018-09-11 19:09:45 +0300

    Added both http and https handlers

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express')
 var fs = require('fs')
 var https = require('https')
-
 var app = express()
 
 app.use(express.static('build'))
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app).listen(80, function() {console.log('Listening on port 80...')})
+var credentials = {
+  key: fs.readFileSync('security/private.key', 'utf8'),
+  cert: fs.readFileSync('security/certificate.crt', 'utf8'),
+  ca: fs.readFileSync('security/ca_bundle.crt', 'utf8'),
+}
+
+var httpServer = http.createServer(app)
+var httpsServer = https.createServer(credentials, app)
+
+httpServer.listen(80)
+httpsServer.listen(443)
