134cad673ca937592fb7ca9a7fb4efc748ce860c
Vlad Trishch <vlad.trishch@gmail.com>
2018-09-27 14:57:52 +0300

    Updated main header text

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -86,7 +86,7 @@ export default compose(injectSheet(({defaultSection, breakpoints, highlightedCol
         <Row>
           <AnimatedLine wrapper={classes.SectionWrapper} queueNumber={3}>
             <p className={classes.Description}>
-              3dmania helps young startups, businesses and just curious people create solutions, utilizing most advanced Digital Manufacturing and IT technologies. Agile Product Development & Rapid prototyping 
+              Agile Product Development & Rapid prototyping → We will gently guide you through the process of creating your dream!
             </p>
           </AnimatedLine>
         </Row>
